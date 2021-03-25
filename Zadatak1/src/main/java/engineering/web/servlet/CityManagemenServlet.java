5a13affb038c7cec3526a468d447fc9840916813
MarijaColic <colic.marija17@gmail.com>
2021-03-25 13:57:40 +0100

    Citypage Prikaz svih gradova i prikaz grada koji odabere korisnik

--- Zadatak1/src/main/java/engineering/web/servlet/CityManagemenServlet.java
+++ Zadatak1/src/main/java/engineering/web/servlet/CityManagemenServlet.java
@@ -1,8 +1,51 @@
 package engineering.web.servlet;
 
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-@WebServlet
+
+import engineering.web.model.CityRepository;
+@WebServlet("/city")
 public class CityManagemenServlet extends HttpServlet{
 
+	protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException ,java.io.IOException {
+		String option = req.getParameter("option");
+		List<CityRepository> cities = (List<CityRepository>) getServletContext().getAttribute("cities");
+		switch (option) {
+		case "Dodaj grad":
+			
+			break;
+		
+		case "Prikazi sve gradove":
+			req.setAttribute("list", cities);
+			req.getRequestDispatcher("/citypage.jsp").forward(req, resp);
+			break;
+	
+		case "Izmeni naziv grada":
+	
+			break;
+		
+		case "Prikaz podataka o gradu":
+			String naziv = req.getParameter("naziv");
+			for(CityRepository c : cities) {
+				if(c.getNaziv().equals(naziv)){
+					
+					req.setAttribute("city", "PTTBroj grada je : " + c.getPTTB() + " , Naziv grada je : " + c.getNaziv());
+					req.getRequestDispatcher("/citypage.jsp").forward(req, resp);
+					}
+			
+				else {
+					req.setAttribute("errorCity", "Ne postoji takav grad u bazi");
+					req.getRequestDispatcher("/citypage.jsp").forward(req, resp);
+				}
+			}
+			break;
+		
+		case "Brisanje grada":
+
+			break;
+		}
+		
+	};
 }
--- Zadatak1/src/main/webapp/index.jsp
+++ Zadatak1/src/main/webapp/index.jsp
@@ -1,7 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@page isELIgnored="false" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <body>
 <h2>Login form</h2>
 	<div>
+		<p>${message}</p>
 		<form action="login" name="Form" method="post">
 			
 			<label for="username">Username</label>
@@ -9,7 +15,7 @@
 			<label for="password">Password</label> 
 			<input type="password" id="password"name="password" placeholder="Enter password" /> 
 			<input type="submit" value="Login" name="Login">
-			
+		
 		</form>
 	</div>
 </body>
